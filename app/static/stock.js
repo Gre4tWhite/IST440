d15be41b4871759a6998a788dd56f7fb643902ad
Tom <tgadola@gmail.com>
2016-11-07 12:41:36 -0500

    [Layout and Function Changes]

--- app/static/stock.js
+++ app/static/stock.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
         url: "https://www.quandl.com/api/v3/datasets/WIKI/ATVI.json?api_key=5cESzn4hsyeJPnz88zci",
         datatype: 'json',
         success: function(data) {
-            var closingTradeStatement = "";
-            closingTradeStatement += JSON.stringify(data.dataset.name) + " closed at ";
+            var closingTradeStatement = "\n";
+            closingTradeStatement += JSON.stringify(data.dataset.dataset_code) + " closed at ";
             closingTradeStatement += JSON.stringify(data.dataset.data[0][4]);
             closingTradeStatement += " on " + JSON.stringify(data.dataset.end_date);
 
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 #!usr/bin/python
 
 from app import app
-app.run(debug=True)
+app.run(debug=True, host='127.0.0.1', port=5000)
